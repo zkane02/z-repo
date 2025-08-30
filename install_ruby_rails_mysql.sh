ed0cc6742e282f7b59ac2000f3fb6c4019c0dd7f
zkane02 <zachkane46@gmail.com>
2025-08-30 13:20:20 -0400

    Add files via upload

---
+++ install_ruby_rails_mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ruby, rails, and mysql
+# homebrew was previously installed
+
+# install Ruby
+brew install ruby
+
+# add Ruby to PATH
+echo 'export PATH="/opt/homebrew/opt/ruby/bin:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+
+# install rails
+gem install rails
+
+# add gem bin to PATH
+echo 'export PATH="/opt/homebrew/lib/ruby/gems/3.4.0/bin:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+
+# install mysql
+brew install mysql
+
+# close and re-open terminal
+# start mysql as a background service
+brew services start mysql
